19f58896d5033c48648c2cb255f1f6bbfbcdba8b
Robin Leroy <egg.robin.leroy@gmail.com>
2015-11-24 22:50:23 +0100

    a tree that works

--- ksp_plugin_adapter/reference_frame_selector.cs
+++ ksp_plugin_adapter/reference_frame_selector.cs
@@ -6,23 +6,40 @@ using System.Text;
 namespace principia {
 namespace ksp_plugin_adapter {
 
+static class CelestialExtensions {
+  public static bool is_leaf(this CelestialBody celestial) {
+    return celestial.orbitingBodies.Count == 0;
+  }
+
+  public static bool is_root(this CelestialBody celestial) {
+    return celestial.orbit == null;
+  }
+}
+
 class ReferenceFrameSelector {
 
   Dictionary<CelestialBody, bool> expanded_;
   CelestialBody selected_celestial_;
 
-
   private void RenderSubtree(CelestialBody celestial, int depth) {
-    bool leaf = celestial.orbitingBodies.Count == 0;
-    bool expanded = expanded_[celestial];
-    UnityEngine.GUILayout.Label("", UnityEngine.GUILayout.Width(10 * depth));
-    UnityEngine.GUILayout.Button(leaf ? "" : (expanded ? "-" : "+"),
-                                 UnityEngine.GUILayout.Width(10));
+    const int offset = 30;
+    UnityEngine.GUILayout.BeginHorizontal();
+    if (!celestial.is_root()) {
+      UnityEngine.GUILayout.Label(
+          "",
+          UnityEngine.GUILayout.Width(offset * (depth - 1)));
+      if (UnityEngine.GUILayout.Button(
+              celestial.is_leaf() ? "" : (expanded_[celestial] ? "-" : "+"),
+              UnityEngine.GUILayout.Width(offset))) {
+        expanded_[celestial] = !expanded_[celestial];
+      }
+    }
     if (UnityEngine.GUILayout.Toggle(selected_celestial_ == celestial,
                                      celestial.name)) {
       selected_celestial_ = celestial;
     }
-    if (expanded) {
+    UnityEngine.GUILayout.EndHorizontal();
+    if (celestial.is_root() || (!celestial.is_leaf() && expanded_[celestial])) {
       foreach (CelestialBody child in celestial.orbitingBodies) {
         RenderSubtree(child, depth + 1);
       }
@@ -32,14 +49,18 @@ class ReferenceFrameSelector {
   public ReferenceFrameSelector() {
     expanded_ = new Dictionary<CelestialBody, bool>();
     foreach (CelestialBody celestial in FlightGlobals.Bodies) {
-      expanded_.Add(celestial, false);
+      if (!celestial.is_leaf() && !celestial.is_root()) {
+        expanded_.Add(celestial, false);
+      }
     }
     selected_celestial_ =
         FlightGlobals.currentMainBody ?? Planetarium.fetch.Sun;
-    for (CelestialBody celestial = selected_celestial_.referenceBody;
-         celestial != null;
-         celestial = celestial.referenceBody) {
-      expanded_[FlightGlobals.currentMainBody] = true;
+    for (CelestialBody celestial = selected_celestial_;
+         celestial.orbit != null;
+         celestial = celestial.orbit.referenceBody) {
+      if (!celestial.is_leaf()) {
+        expanded_[celestial] = true;
+      }
     }
   }
 
@@ -49,7 +70,7 @@ class ReferenceFrameSelector {
     UnityEngine.GUILayout.BeginVertical();
     RenderSubtree(celestial : Planetarium.fetch.Sun, depth : 0);
     UnityEngine.GUILayout.EndVertical();
-    UnityEngine.GUI.skin = old_skin
+    UnityEngine.GUI.skin = old_skin;
   }
 }
 
