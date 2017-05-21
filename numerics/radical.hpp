03c548aca03c34d281833674f9598b1a877368e7
Robin Leroy <egg.robin.leroy@gmail.com>
2017-05-21 18:55:41 +0200

    Abs, Pow, equality

--- numerics/radical.hpp
+++ numerics/radical.hpp
@@ -1,6 +1,8 @@
 ﻿
 #pragma once
 
+#include <type_traits>
+
 #include "quantities/elementary_functions.hpp"
 
 namespace principia {
@@ -9,15 +11,17 @@ namespace radical {
 namespace internal_radical {
 
 using quantities::Angle;
+using quantities::Exponentiation;
 using quantities::Product;
 using quantities::Quantity;
 using quantities::Quotient;
+using quantities::Sqrt;
 using quantities::SquareRoot;
 
 template<typename Q>
 class Radical {
  public:
-  constexpr Radical(Q const& real_part) : value_{real_part} {}
+  constexpr Radical(Q const& real_part);
   Q real_part() const;
   Q imaginary_part() const;
  private:
@@ -30,19 +34,20 @@ class Radical {
   friend constexpr Radical<double> Sqrt(double x);
 };
 
-// This is only constexpr when x = -1.  This allows |i| to be constexpr.  The
-// implementation is inline to allow |i| to be defined below.
-constexpr Radical<double> Sqrt(double x) {
-  return x == -1 ? Radical<double>{/*imaginary=*/true, 1}
-                 : std::signbit(x)
-                       ? Radical<double>{/*imaginary=*/true, std::sqrt(-x)}
-                       : std::sqrt(x);
-}
+template<typename Q>
+Q Abs(Radical<Q> const& z);
 
+// The behaviour of |Sqrt| is correct with respect to the sign of 0, i.e.,
+// Sqrt(-0) is +0 * i.
 template<typename D>
 Radical<SquareRoot<Quantity<D>>> Sqrt(Quantity<D> const& x);
+// This is only constexpr when x = -1, allowing |i| to be constexpr.
+constexpr Radical<double> Sqrt(double x);
 
-constexpr Radical<double> i = Sqrt(-1);
+template<int exponent,
+         typename Q,
+         typename = std::enable_if_t<exponent % 2 == 0>>
+Exponentiation<Q, exponent> Pow(Radical<Q> z);
 
 template<typename Q>
 Radical<Q>& operator*=(Radical<Q>& left, double right);
@@ -65,25 +70,56 @@ template<typename Q1, typename Q2>
 Radical<Product<Q1, Q2>> operator/(Radical<Q1> const& left,
                                    Radical<Q2> const& right);
 
+// Equality is correct with respect to the sign of 0, i.e., -0, +0, -0 * i, and
+// +0 * i are equal.
+template<typename Q>
+bool operator==(Radical<Q> const& left, Radical<Q> const& right);
+template<typename Q>
+bool operator!=(Radical<Q> const& left, Radical<Q> const& right);
+
 // Argument-dependent lookup will find the following functions when using
-// |Radical| arguments.  When starting from quantities, using |radical::Sin(x)|
-// etc. will convert to |Radical|.
+// |Radical| arguments.  When starting from real quantities, using
+// |radical::Sin(x)| etc. will convert to |Radical|.
 
 Radical<double> Sin(Radical<Angle> const& z);
 Radical<double> Cos(Radical<Angle> const& z);
 Radical<double> Tan(Radical<Angle> const& z);
 
-Radical<Angle> ArcSin(Radical<double> z);
-Radical<Angle> ArcCos(Radical<double> z);
-Radical<Angle> ArcTan(Radical<double> w, Radical<double> z = 1);
+Radical<Angle> ArcSin(Radical<double> const& z);
+Radical<Angle> ArcCos(Radical<double> const& z);
+Radical<Angle> ArcTan(Radical<double> const& w, Radical<double> const& z = 1);
 
 Radical<double> Sinh(Radical<Angle> const& z);
 Radical<double> Cosh(Radical<Angle> const& z);
 Radical<double> Tanh(Radical<Angle> const& z);
 
-Radical<Angle> ArcSinh(Radical<double> z);
-Radical<Angle> ArcCosh(Radical<double> z);
-Radical<Angle> ArcTanh(Radical<double> z);
+Radical<Angle> ArcSinh(Radical<double> const& z);
+Radical<Angle> ArcCosh(Radical<double> const& z);
+Radical<Angle> ArcTanh(Radical<double> const& z);
+
+// Some implementations to allow |i| to be defined constexpr below.
+
+template<typename Q>
+constexpr Radical<Q>::Radical(Q const& real_part) : value_{real_part} {}
+
+template<typename Q>
+constexpr Radical<Q>::Radical(bool imaginary, Q const& value)
+    : imaginary_{imaginary}, value_{value} {}
+
+constexpr Radical<double> Sqrt(double x) {
+  return x == -1
+             ? Radical<double>{/*imaginary=*/true, 1}
+             : std::signbit(x) ? Radical<double>{/*imaginary=*/true, Sqrt(-x)}
+                               : Sqrt(x);
+}
+
+constexpr Radical<double> i = Sqrt(-1);
+
+template<typename Q>
+std::string DebugString(Radical<Q> const& z);
+
+template<typename Q>
+std::ostream& operator<<(std::ostream& out, Radical<Q> const& z);
 
 }  // namespace internal_radical
 
