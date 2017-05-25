2c6136d9015e915871df9a19719b4b0284a11dd6
Robin Leroy <egg.robin.leroy@gmail.com>
2017-05-25 16:39:24 +0200

    rename

--- numerics/radical.hpp
+++ numerics/radical.hpp
@@ -19,110 +19,122 @@ using quantities::Sqrt;
 using quantities::SquareRoot;
 
 template<typename Q>
-class Radical {
+class PossiblyImaginary {
  public:
-  constexpr Radical(Q const& real_part);
-  Q real_part() const;
-  Q imaginary_part() const;
+  constexpr PossiblyImaginary(Q const& real_part);  // NOLINT(runtime/explicit)
+  constexpr real_part() const;
+  constexpr imaginary_part() const;
  private:
-  constexpr Radical(bool imaginary, Q const& value)
-      : imaginary_{imaginary}, value_{value} {}
+  constexpr PossiblyImaginary(bool imaginary, Q const& value);
   bool imaginary_;
   // If |imaginary_|, the imaginary part, otherwise, the real part.
   Q value_;
 
-  friend constexpr Radical<double> Sqrt(double x);
+  friend constexpr PossiblyImaginary<double> Sqrt(double x);
 };
 
 template<typename Q>
-Q Abs(Radical<Q> const& z);
+Q Abs(PossiblyImaginary<Q> const& z);
 
 // The behaviour of |Sqrt| is correct with respect to the sign of 0, i.e.,
 // Sqrt(-0) is +0 * i.
 template<typename D>
-Radical<SquareRoot<Quantity<D>>> Sqrt(Quantity<D> const& x);
+PossiblyImaginary<SquareRoot<Quantity<D>>> Sqrt(Quantity<D> const& x);
 // This is only constexpr when x = -1, allowing |i| to be constexpr.
-constexpr Radical<double> Sqrt(double x);
+constexpr PossiblyImaginary<double> Sqrt(double x);
 
 template<int exponent,
          typename Q,
          typename = std::enable_if_t<exponent % 2 == 0>>
-Exponentiation<Q, exponent> Pow(Radical<Q> z);
+Exponentiation<Q, exponent> Pow(PossiblyImaginary<Q> z);
 
 // Equality is correct with respect to the sign of 0, i.e., -0, +0, -0 * i, and
 // +0 * i are equal.
 template<typename Q>
-bool operator==(Radical<Q> const& left, Radical<Q> const& right);
+bool operator==(PossiblyImaginary<Q> const& left,
+                PossiblyImaginary<Q> const& right);
 template<typename Q>
-bool operator!=(Radical<Q> const& left, Radical<Q> const& right);
+bool operator!=(PossiblyImaginary<Q> const& left,
+                PossiblyImaginary<Q> const& right);
 
 template<typename Q>
-Radical<Q> operator-(Radical<Q> const& z);
+PossiblyImaginary<Q> operator-(PossiblyImaginary<Q> const& z);
 
 template<typename Q1, typename Q2>
-Radical<Product<Q1, Q2>> operator*(Radical<Q1> const& left, Q2 const& right);
+PossiblyImaginary<Product<Q1, Q2>> operator*(PossiblyImaginary<Q1> const& left,
+                                             Q2 const& right);
 template<typename Q1, typename Q2>
-Radical<Product<Q1, Q2>> operator*(Q1 const& left, Radical<Q2> const& right);
+PossiblyImaginary<Product<Q1, Q2>> operator*(
+    Q1 const& left,
+    PossiblyImaginary<Q2> const& right);
 template<typename Q1, typename Q2>
-Radical<Product<Q1, Q2>> operator*(Radical<Q1> const& left,
-                                   Radical<Q2> const& right);
+PossiblyImaginary<Product<Q1, Q2>> operator*(
+    PossiblyImaginary<Q1> const& left,
+    PossiblyImaginary<Q2> const& right);
 
 template<typename Q1, typename Q2>
-Radical<Product<Q1, Q2>> operator/(Radical<Q1> const& left, Q2 const& right);
+PossiblyImaginary<Product<Q1, Q2>> operator/(PossiblyImaginary<Q1> const& left,
+                                             Q2 const& right);
 template<typename Q1, typename Q2>
-Radical<Product<Q1, Q2>> operator/(Q1 const& left, Radical<Q2> const& right);
+PossiblyImaginary<Product<Q1, Q2>> operator/(
+    Q1 const& left,
+    PossiblyImaginary<Q2> const& right);
 template<typename Q1, typename Q2>
-Radical<Product<Q1, Q2>> operator/(Radical<Q1> const& left,
-                                   Radical<Q2> const& right);
+PossiblyImaginary<Product<Q1, Q2>> operator/(
+    PossiblyImaginary<Q1> const& left,
+    PossiblyImaginary<Q2> const& right);
 
 template<typename Q>
-Radical<Q>& operator*=(Radical<Q>& left, double right);
+PossiblyImaginary<Q>& operator*=(PossiblyImaginary<Q>& left, double right);
 template<typename Q>
-Radical<Q>& operator/=(Radical<Q>& left, double right);
+PossiblyImaginary<Q>& operator/=(PossiblyImaginary<Q>& left, double right);
 
 // Argument-dependent lookup will find the following functions when using
-// |Radical| arguments.  When starting from real quantities, using
-// |radical::Sin(x)| etc. will convert to |Radical|.
+// |PossiblyImaginary| arguments.  When starting from real quantities, using
+// |radical::Sin(x)| etc. will convert to |PossiblyImaginary|.
 
-Radical<double> Sin(Radical<Angle> const& z);
-Radical<double> Cos(Radical<Angle> const& z);
-Radical<double> Tan(Radical<Angle> const& z);
+PossiblyImaginary<double> Sin(PossiblyImaginary<Angle> const& z);
+PossiblyImaginary<double> Cos(PossiblyImaginary<Angle> const& z);
+PossiblyImaginary<double> Tan(PossiblyImaginary<Angle> const& z);
 
-Radical<Angle> ArcSin(Radical<double> const& z);
-Radical<Angle> ArcCos(Radical<double> const& z);
-Radical<Angle> ArcTan(Radical<double> const& w, Radical<double> const& z = 1);
+PossiblyImaginary<Angle> ArcSin(PossiblyImaginary<double> const& z);
+PossiblyImaginary<Angle> ArcCos(PossiblyImaginary<double> const& z);
+PossiblyImaginary<Angle> ArcTan(PossiblyImaginary<double> const& w,
+                                PossiblyImaginary<double> const& z = 1);
 
-Radical<double> Sinh(Radical<Angle> const& z);
-Radical<double> Cosh(Radical<Angle> const& z);
-Radical<double> Tanh(Radical<Angle> const& z);
+PossiblyImaginary<double> Sinh(PossiblyImaginary<Angle> const& z);
+PossiblyImaginary<double> Cosh(PossiblyImaginary<Angle> const& z);
+PossiblyImaginary<double> Tanh(PossiblyImaginary<Angle> const& z);
 
-Radical<Angle> ArcSinh(Radical<double> const& z);
-Radical<Angle> ArcCosh(Radical<double> const& z);
-Radical<Angle> ArcTanh(Radical<double> const& z);
+PossiblyImaginary<Angle> ArcSinh(PossiblyImaginary<double> const& z);
+PossiblyImaginary<Angle> ArcCosh(PossiblyImaginary<double> const& z);
+PossiblyImaginary<Angle> ArcTanh(PossiblyImaginary<double> const& z);
 
 // Some implementations to allow |i| to be defined constexpr below.
 
 template<typename Q>
-constexpr Radical<Q>::Radical(Q const& real_part) : value_{real_part} {}
+constexpr PossiblyImaginary<Q>::PossiblyImaginary(Q const& real_part)
+    : value_{real_part} {}
 
 template<typename Q>
-constexpr Radical<Q>::Radical(bool imaginary, Q const& value)
+constexpr PossiblyImaginary<Q>::PossiblyImaginary(bool imaginary,
+                                                  Q const& value)
     : imaginary_{imaginary}, value_{value} {}
 
-constexpr Radical<double> Sqrt(double x) {
-  return x == -1
-             ? Radical<double>{/*imaginary=*/true, 1}
-             : std::signbit(x) ? Radical<double>{/*imaginary=*/true, Sqrt(-x)}
-                               : Sqrt(x);
+constexpr PossiblyImaginary<double> Sqrt(double x) {
+  return x == -1 ? PossiblyImaginary<double>{/*imaginary=*/true, 1}
+                 : std::signbit(x)
+                       ? PossiblyImaginary<double>{/*imaginary=*/true, Sqrt(-x)}
+                       : Sqrt(x);
 }
 
-constexpr Radical<double> i = Sqrt(-1);
+constexpr PossiblyImaginary<double> i = Sqrt(-1);
 
 template<typename Q>
-std::string DebugString(Radical<Q> const& z);
+std::string DebugString(PossiblyImaginary<Q> const& z);
 
 template<typename Q>
-std::ostream& operator<<(std::ostream& out, Radical<Q> const& z);
+std::ostream& operator<<(std::ostream& out, PossiblyImaginary<Q> const& z);
 
 }  // namespace internal_radical
 
