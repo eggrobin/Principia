7cb87ed37ff33d7afbb5a561592b84e53f8b88f5
Robin Leroy <egg.robin.leroy@gmail.com>
2018-12-25 16:51:59 +0100

    try noinline

--- physics/geopotential_body.hpp
+++ physics/geopotential_body.hpp
@@ -168,23 +168,23 @@ template<typename Frame>
 template<int degree, int... orders>
 struct Geopotential<Frame>::
 DegreeNAllOrders<degree, std::integer_sequence<int, orders...>> {
-  static auto Acceleration(Geopotential<Frame> const& geopotential,
-                                  Vector<double, Frame> const& r_normalized,
-                                  Length const& r_norm,
-                                  Square<Length> const& r²,
-                                  Precomputations& precomputations)
-      -> Vector<ReducedAcceleration, Frame>;
+  static __declspec(noinline) auto Acceleration(
+      Geopotential<Frame> const& geopotential,
+      Vector<double, Frame> const& r_normalized,
+      Length const& r_norm,
+      Square<Length> const& r²,
+      Precomputations& precomputations) -> Vector<ReducedAcceleration, Frame>;
 };
 
 template<typename Frame>
 template<int... degrees>
 struct Geopotential<Frame>::AllDegrees<std::integer_sequence<int, degrees...>> {
   static auto Acceleration(Geopotential<Frame> const& geopotential,
-                                  Instant const& t,
-                                  Displacement<Frame> const& r,
-                                  Length const& r_norm,
-                                  Square<Length> const& r²,
-                                  Exponentiation<Length, -3> const& one_over_r³)
+                           Instant const& t,
+                           Displacement<Frame> const& r,
+                           Length const& r_norm,
+                           Square<Length> const& r²,
+                           Exponentiation<Length, -3> const& one_over_r³)
       -> Vector<ReducedAcceleration, Frame>;
 };
 
