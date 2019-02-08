e946302cae998a6d3d53fbc38ea11689a06f76b8
Robin Leroy <egg.robin.leroy@gmail.com>
2019-02-09 00:23:01 +0100

    TLE-to-instantaneous-state is just bad

--- astronomy/orbit_analyzer_test.cpp
+++ astronomy/orbit_analyzer_test.cpp
@@ -129,7 +129,7 @@ TEST_F(OrbitAnalyzerTest, 福爾摩沙衛星二號) {
       Velocity<ICRS>({-0.00060951 * AstronomicalUnit / Day,
                       -0.00285516 * AstronomicalUnit / Day,
                       0.00312683 * AstronomicalUnit / Day})};
-#else
+#elif 0
   // Still operational.
   // 1 28254U 04018A   16183.13910299  .00000106  00000-0  10000-3 0  9998
   // 2 28254  98.9332 246.1213 0002410  82.2354 330.6801 14.00730683619455
@@ -140,8 +140,18 @@ TEST_F(OrbitAnalyzerTest, 福爾摩沙衛星二號) {
                           3.82473828e-05 * AstronomicalUnit}),
       Velocity<ICRS>({0.00103147 * AstronomicalUnit / Day,
                       0.00327939 * AstronomicalUnit / Day,
-                      0.00254849 * AstronomicalUnit / Day})*(1 - .00015)};
-  
+                      0.00254849 * AstronomicalUnit / Day})*(1 - .00014)};
+#else
+  // 1 28254U 04018A   10001.06515326  .00000104  00000-0  10000-3 0  3571
+  // 2 28254  99.1103  64.2419 0002824 109.6874   2.6228 14.00732798287225
+  auto const epoch = "2010-01-01T01:33:49"_UTC;
+  RelativeDegreesOfFreedom<ICRS> const satellite_state_vectors = {
+      Displacement<ICRS>({-1.61566730e-06 * AstronomicalUnit,
+                          -1.97102559e-05 * AstronomicalUnit,
+                          4.43241251e-05 * AstronomicalUnit}),
+      Velocity<ICRS>({-0.00196118 * AstronomicalUnit / Day,
+                      -0.00344896 * AstronomicalUnit / Day,
+                      -0.00160197 * AstronomicalUnit / Day})};
 #endif
   ephemeris_->Prolong(epoch);
   auto const earth_degrees_of_freedom =
